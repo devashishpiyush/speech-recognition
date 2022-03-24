b480fbfe1731a5e46f377d6552d323620fdf0ea1
unknown <medevashishroy@gmail.com>
2022-03-24 14:38:54 +0530

    feature: voice recognition and voice recording
    voice recognition with transcript and voice recording with auto download to local computer

--- src/Components/MicSystem/micSystem.js
+++ src/Components/MicSystem/micSystem.js
@@ -1,7 +1,72 @@
 import "./micSystem.css";
+import { useState } from "react";
+import { useReactMediaRecorder } from "react-media-recorder";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import { saveAs } from "file-saver";
 
 const MicSystem = () => {
-    const listening = false;
+    const [script, setScript] = useState([]);
+    const [micState, setMicState] = useState(true);
+
+    const {
+        listening,
+        transcript,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
+
+    const {
+        status,
+        startRecording,
+        stopRecording,
+        mediaBlobUrl,
+        clearBlobUrl
+    } = useReactMediaRecorder({audio: true});
+
+    const switchMic = () => {
+        setMicState((preStatus) => {
+            return !preStatus;
+        });
+
+        if (micState === true){
+            start(); 
+        } else if (micState === false) {
+            stop();
+        }
+    }
+
+    const start = () => {
+        startRecording();
+        SpeechRecognition.startListening({
+            continuous: true
+        });
+    }
+
+    const stop = async () => {
+        stopRecording();
+        SpeechRecognition.stopListening();
+
+        // we set a time dealy of 20 sec, so that the module can process the voice
+        // and produce the transcript.
+        setTimeout(() => {
+            setScript([...script, transcript]);
+            console.log("20 sec passed, transcript processing might be completed.");
+        }, 20000);
+    }
+
+    const clear = () => {  
+        saveAs(mediaBlobUrl, "SpeechAudio.wav");
+        resetTranscript();
+        clearBlobUrl();
+    }
+
+    const log = () => { 
+        console.log(script);
+    }
+
+    if(!browserSupportsSpeechRecognition) {
+        return (<span>Unsupported Browser</span>);
+    }
 
     return (
         <>
@@ -11,11 +76,13 @@ const MicSystem = () => {
                         <div className="row">
                             <div className="col-3"></div>
                             <div className="col-6">
-                                <button className="btn btn-primary" ><span className="fa-solid fa-microphone fa-5x"></span></button>
-                                <h4>Microphone: Off</h4>
-                                <p>Transcript goes here</p><br />
-                                <button className="btn btn-sm btn-danger mx-3 px-3">Clear</button>
-                                <button className="btn btn-sm btn-primary mx-3 px-3">Show Log</button>
+                            <audio src={mediaBlobUrl} controls autoPlay /><br />
+                                {mediaBlobUrl}<br />
+                                <button className="btn btn-primary" onClick={switchMic}><span className="fa-solid fa-microphone fa-5x"></span></button>
+                                <h4>Microphone: {listening ? 'On' : 'Off'} & <small>{status.toUpperCase()}</small></h4>
+                                <p>{transcript === "" ? <br /> : transcript }</p>
+                                <button className="btn btn-sm btn-danger mx-3 px-3" onClick={clear}>Clear</button>
+                                <button className="btn btn-sm btn-primary mx-3 px-3" onClick={log}>Show Log</button>
                                 <br />
                             </div>
                         </div>
